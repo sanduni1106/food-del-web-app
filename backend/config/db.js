a9e51b17174c409255096ea5d9dc93bb64b2ea59
sanduni1106 <sandunihirushika52@gmail.com>
2024-09-02 09:40:58 +0530

    db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://sanduni:768878812@cluster0.mdvs7vl.mongodb.net/food-del').then(()=>console.log("DB Connected"));
+    await mongoose.connect('').then(()=>console.log("DB Connected"));
 }
